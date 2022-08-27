8c6ea7481a81d97a909bd854a63de2cbce3458c2
Omid <omidsaneico@gmail.com>
2022-08-27 12:47:11 +0430

    Add admin tickets rout group

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -17,15 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::prefix('admin')->group(function () {
-    Route::get('/login', function(){
 
-    });
-    Route::get('/logout', function(){
+Route::prefix('admin')->group(function() {
+    Route::prefix('tickets')->group(function() {
+        Route::get('/', [TicketController::class, 'create']);
+        Route::get('create', [TicketController::class, 'create']);
+        Route::post('store', [TicketController::class, 'store']);
+        Route::get('show', [TicketController::class, 'show']);
+        Route::get('edit', [TicketController::class, 'edit']);
+        Route::patch('update', [TicketController::class, 'update']);
+        Route::delete('destroy', [TicketController::class, 'destroy']);
+
 
     });
 });
-
 
 Auth::routes();
 
