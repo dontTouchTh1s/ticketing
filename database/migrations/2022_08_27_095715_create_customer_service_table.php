73023655d9249df5ea82ffd67472516e2b992b1a
Mohammad Mahdi <m.mahdi.m32@gmail.com>
2022-08-27 17:09:23 +0430

    initial migration

---
+++ database/migrations/2022_08_27_095715_create_customer_service_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_service', function (Blueprint $table) {
+            $table->integer('customer_id');
+            $table->integer('service_id');
+            $table->dateTime('started_at');
+            $table->dateTime('end_at');
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_service');
+    }
+};
---
+++ database/migrations/2022_08_27_102709_create_tickets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained();
+            $table->foreignId('service_id')->constrained();
+            $table->foreignId('department_id')->constrained();
+            $table->string('subject');
+            $table->longText('content');
+            $table->integer('priority');
+            $table->boolean('active');
+            $table->string('ip');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
