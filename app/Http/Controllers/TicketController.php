d1d642b48782ff8b53c72eab7fe758e5a953ab0b
Mohammad Mahdi <m.mahdi.m32@gmail.com>
2022-08-29 16:42:11 +0430

    Add ticket model to db in store method, and get service and department data in create method

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,20 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Department;
+use App\Models\Service;
 use App\Models\Ticket;
 use App\Http\Requests\StoreTicketRequest;
 use App\Http\Requests\UpdateTicketRequest;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Request;
 
 class TicketController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Contracts\View\View
+     * @return View
      */
     public function index()
     {
-        return view('admin.tickets.show_tickets', ['tickets' => Ticket::all()] );
+        $tickets = Ticket::all();
+        return view('admin.tickets.show_tickets', ['tickets' => $tickets] );
     }
 
     /**
@@ -25,18 +30,47 @@ class TicketController extends Controller
      */
     public function create()
     {
-        //
+        $departments = Department::all();
+        $department_info = [];
+        foreach ($departments as $department)
+        {
+            array_push($department_info, $department->attributesToArray());
+        }
+        $services = Service::all();
+        $service_info = [];
+        foreach ($services as $service)
+        {
+            array_push($service_info, $service->attributesToArray());
+        }
+
+        return view('admin.tickets.create_ticket')
+            ->with([
+                'services' => $service_info,
+                'departments' => $department_info]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StoreTicketRequest  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(StoreTicketRequest $request)
     {
-        //
+        $validated = $request->validated();
+        $ticket = [
+            'service_id' => $validated['service_id'],
+            'customer_id' => '2',
+            'department_id' => $validated['department_id'],
+            'subject' => $validated['subject'],
+            'content' => $validated['content'],
+            'active' => true,
+            'priority' => 1,
+            'ip' => Request::ip(),
+
+        ];
+        Ticket::create($ticket);
+        return redirect()->route('ticket.create');
     }
 
     /**
