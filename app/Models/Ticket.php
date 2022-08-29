7da025fa1b8fbbe251e2705569bd0cd3efe5c8a5
Mohammad Mahdi <m.mahdi.m32@gmail.com>
2022-08-29 16:45:50 +0430

    Add fillable property

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'customer_id',
+        'service_id',
+        'department_id',
+        'active',
+        'priority',
+        'ip',
+        'subject',
+        'content'
+        ];
+
 }
