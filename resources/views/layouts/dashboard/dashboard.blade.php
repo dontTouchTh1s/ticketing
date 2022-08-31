65c47a10772a1d20b1a52d174fe29b51c4a2460f
Ali Mashyekhi <alim201381@gmail.com>
2022-08-31 04:42:36 -0700

    Use adminLTE dashboard

--- resources/views/layouts/dashboard/dashboard.blade.php
+++ resources/views/layouts/dashboard/dashboard.blade.php
@@ -12,14 +12,14 @@
     <!-- IonIcons -->
     <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="resources/css/adminlte.min.css">
+    <link rel="stylesheet" href="/css/adminlte.min.css">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
     <!-- bootstrap rtl -->
-    <link rel="stylesheet" href="resources/css/bootstrap-rtl.min.css">
+    <link rel="stylesheet" href="/css/bootstrap-rtl.min.css">
     <!-- template rtl version -->
-    <link rel="stylesheet" href="resources/css/custom-style.css">
+    <link rel="stylesheet" href="/css/custom-style.css">
 
 </head>
 <!--
@@ -35,132 +35,14 @@ to get the desired effect
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
     <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="index3.html" class="nav-link">خانه</a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="#" class="nav-link">تماس</a>
-            </li>
-        </ul>
-
-        <!-- SEARCH FORM -->
-        <form class="form-inline ml-3">
-            <div class="input-group input-group-sm">
-                <input class="form-control form-control-navbar" type="search" placeholder="جستجو" aria-label="Search">
-                <div class="input-group-append">
-                    <button class="btn btn-navbar" type="submit">
-                        <i class="fa fa-search"></i>
-                    </button>
-                </div>
-            </div>
-        </form>
-
-        <!-- Right navbar links -->
-        <ul class="navbar-nav mr-auto">
-            <!-- Messages Dropdown Menu -->
-            <li class="nav-item dropdown">
-                <a class="nav-link" data-toggle="dropdown" href="#">
-                    <i class="fa fa-comments-o"></i>
-                    <span class="badge badge-danger navbar-badge">3</span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-left">
-                    <a href="#" class="dropdown-item">
-                        <!-- Message Start -->
-                        <div class="media">
-                            <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 ml-3 img-circle">
-                            <div class="media-body">
-                                <h3 class="dropdown-item-title">
-                                    حسام موسوی
-                                    <span class="float-left text-sm text-danger"><i class="fa fa-star"></i></span>
-                                </h3>
-                                <p class="text-sm">با من تماس بگیر لطفا...</p>
-                                <p class="text-sm text-muted"><i class="fa fa-clock-o mr-1"></i> 4 ساعت قبل</p>
-                            </div>
-                        </div>
-                        <!-- Message End -->
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <!-- Message Start -->
-                        <div class="media">
-                            <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle ml-3">
-                            <div class="media-body">
-                                <h3 class="dropdown-item-title">
-                                    پیمان احمدی
-                                    <span class="float-left text-sm text-muted"><i class="fa fa-star"></i></span>
-                                </h3>
-                                <p class="text-sm">من پیامتو دریافت کردم</p>
-                                <p class="text-sm text-muted"><i class="fa fa-clock-o mr-1"></i> 4 ساعت قبل</p>
-                            </div>
-                        </div>
-                        <!-- Message End -->
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <!-- Message Start -->
-                        <div class="media">
-                            <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle ml-3">
-                            <div class="media-body">
-                                <h3 class="dropdown-item-title">
-                                    سارا وکیلی
-                                    <span class="float-left text-sm text-warning"><i class="fa fa-star"></i></span>
-                                </h3>
-                                <p class="text-sm">پروژه اتون عالی بود مرسی واقعا</p>
-                                <p class="text-sm text-muted"><i class="fa fa-clock-o mr-1"></i>4 ساعت قبل</p>
-                            </div>
-                        </div>
-                        <!-- Message End -->
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item dropdown-footer">مشاهده همه پیام‌ها</a>
-                </div>
-            </li>
-            <!-- Notifications Dropdown Menu -->
-            <li class="nav-item dropdown">
-                <a class="nav-link" data-toggle="dropdown" href="#">
-                    <i class="fa fa-bell-o"></i>
-                    <span class="badge badge-warning navbar-badge">15</span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-left">
-                    <span class="dropdown-item dropdown-header">15 نوتیفیکیشن</span>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <i class="fa fa-envelope ml-2"></i> 4 پیام جدید
-                        <span class="float-left text-muted text-sm">3 دقیقه</span>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <i class="fa fa-users ml-2"></i> 8 درخواست دوستی
-                        <span class="float-left text-muted text-sm">12 ساعت</span>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <i class="fa fa-file ml-2"></i> 3 گزارش جدید
-                        <span class="float-left text-muted text-sm">2 روز</span>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item dropdown-footer">مشاهده همه نوتیفیکیشن</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i
-                        class="fa fa-th-large"></i></a>
-            </li>
-        </ul>
-    </nav>
+    @include('layouts.dashboard.header')
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="index3.html" class="brand-link">
-            <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+            <img src="/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
             <span class="brand-text font-weight-light">پنل مدیریت</span>
         </a>
@@ -575,7 +457,7 @@ to get the desired effect
                                     <tbody>
                                     <tr>
                                         <td>
-                                            <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                                            <img src="/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
                                             تلویزیون هوشمند
                                         </td>
                                         <td>13 تومان</td>
@@ -594,7 +476,7 @@ to get the desired effect
                                     </tr>
                                     <tr>
                                         <td>
-                                            <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                                            <img src="/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
                                             محصول ایکس
                                         </td>
                                         <td>29 تومان</td>
@@ -613,7 +495,7 @@ to get the desired effect
                                     </tr>
                                     <tr>
                                         <td>
-                                            <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                                            <img src="/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
                                             محصول پرفروش
                                         </td>
                                         <td>1,230 تومان</td>
@@ -632,7 +514,7 @@ to get the desired effect
                                     </tr>
                                     <tr>
                                         <td>
-                                            <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
+                                            <img src="/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
                                             محصول جدید
                                             <span class="badge bg-danger">جدید</span>
                                         </td>
@@ -771,15 +653,15 @@ to get the desired effect
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE -->
-<script src="dist/js/adminlte.js"></script>
+<script src="/js/adminlte.js"></script>
 
 <!-- OPTIONAL SCRIPTS -->
-<script src="plugins/chart.js/Chart.min.js"></script>
-<script src="dist/js/demo.js"></script>
-<script src="dist/js/pages/dashboard3.js"></script>
+<script src="/plugins/chart.js/Chart.min.js"></script>
+<script src="/js/demo.js"></script>
+<script src="/js/pages/dashboard3.js"></script>
 </body>
 </html>
