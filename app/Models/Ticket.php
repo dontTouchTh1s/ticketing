53b8185f21a2cf0f276d3d9ed7109b619229fb40
Ali Mashyekhi <alim201381@gmail.com>
2022-09-03 05:19:46 -0700

    Add inverse relationship with other models

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -5,9 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static create(array $ticket)
+ */
 class Ticket extends Model
 {
     use HasFactory;
+
     protected $fillable = [
         'customer_id',
         'service_id',
@@ -17,6 +21,20 @@ class Ticket extends Model
         'ip',
         'subject',
         'content'
-        ];
+    ];
 
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
