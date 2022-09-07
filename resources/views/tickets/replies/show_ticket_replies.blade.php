9ad8e8d62e5558fb8a045308ce2778a7a97a11fe
Ali Mashyekhi <alim201381@gmail.com>
2022-09-07 05:15:53 -0700

    Add (send reply) section to page, add send report modal

--- resources/views/tickets/replies/show_ticket_replies.blade.php
+++ resources/views/tickets/replies/show_ticket_replies.blade.php
@@ -72,7 +72,7 @@
                                         </div>
                                     </form>
                                 </div>
-                                <div class="card card-secondary">
+                                <div class="card card-secondary card-replies">
                                     <div class="card-header">
                                         <h3 class="card-title">
                                             پاسخ ها
@@ -82,11 +82,30 @@
                                         @foreach($replies as $reply)
                                             <div class="row {{ __($reply['justify']) }}">
                                                 <div class="col-lg-6">
-                                                    <div class="card card-secondary">
+                                                    <div class="reply" id="{{ __($reply['id']) }}">
                                                         <div class="reply-header">
                                                             <h5 class="reply-title">
                                                                 {{ __($reply['sender']->name) }}
                                                             </h5>
+                                                            <div class="dropdown">
+                                                                <button class="reply-more-info dropdown-toggle"
+                                                                        type="button" id="dropdownMenuButton"
+                                                                        data-toggle="dropdown"
+                                                                        aria-haspopup="true"
+                                                                        aria-expanded="false">
+                                                                    ...
+                                                                </button>
+                                                                <div class="dropdown-menu"
+                                                                     aria-labelledby="dropdownMenuButton">
+                                                                    <a class="dropdown-item" href="#">یچیزی</a>
+                                                                    <a class="dropdown-item" href="#">کپی</a>
+                                                                    <a class="dropdown-item report-reply" href="#"
+                                                                       data-toggle="modal"
+                                                                       data-target="#reportReplyModal">گزارش</a>
+                                                                </div>
+                                                            </div>
+
+
                                                         </div>
                                                         <div class="reply-body">
                                                             {{ __($reply['content']) }}
@@ -97,6 +116,20 @@
 
                                         @endforeach
                                     </div>
+                                    <div class="add-reply">
+                                        <form class="add-reply" method="post" action="{{ route('replies.create') }}">
+                                            @csrf
+                                            <input type="hidden" name="ticket" value="{{ __($ticket['id']) }}">
+                                            <div id="editor">
+                                                <p>متن پاسخ خود را بنویسید ...</p>
+                                            </div>
+
+
+                                            <button class="btn btn-primary btn-reply-send">
+                                                ارسال
+                                            </button>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -104,5 +137,61 @@
                 </div>
             </div>
         </div>
+        <!-- Modal -->
+        <div class="modal fade" id="reportReplyModal" tabindex="-1" aria-labelledby="reportReplyModal"
+             aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="reportReplyModal">گزارش</h5>
+                        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p/>در صورت مشاهده محتوای خلاف قوانین یا مواردی که نیاز به اطلاع رسانی به مدیر دارد میتوانید از
+                        این قسمت گزارش دهید.
+                        <form action="{{ route('replies.reports.create')  }}" method="POST"
+                              id="modal-form">
+                            @csrf
+
+                            <input type="hidden" id="reply-id" name="reply">
+                            <input type="hidden" id="ticket" name="ticket" value="{{ __($ticket['id']) }}">
+                            <div class="form-group">
+                                <label for="content">
+                                    متن گزارش
+                                </label>
+                                <textarea class="form-control" name="content" id="content" rows="5"></textarea>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">بستن</button>
+
+                        <button type="submit" class="btn btn-primary" id="modal-form-submit">
+                            ارسال
+                        </button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    <script src="/plugins/ckeditor/ckeditor.js"></script>
+    <script>
+
+        ClassicEditor
+            .create(document.querySelector('#editor'), {
+                toolbar: ['bold', 'italic', 'alignment', 'bulletedList', 'numberedList'],
+                list: {
+                    properties: {
+                        styles: true,
+                        startIndex: true,
+                        reversed: true
+                    }
+                },
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    </script>
 @endsection
+
