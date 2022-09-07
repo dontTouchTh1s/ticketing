6747098abde16d0367b37930f997a9b772c74217
Ali Mashyekhi <alim201381@gmail.com>
2022-09-07 03:34:41 -0700

    Separate ticket controller to 2 section

--- app/Http/Controllers/Ticket/TicketController.php
+++ app/Http/Controllers/Ticket/TicketController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Ticket;
 
-use App\Http\Requests\ChangeDepartemntTicketRequest;
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTicketRequest;
 use App\Models\Department;
 use App\Models\Service;
@@ -12,7 +12,6 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class TicketController extends Controller
 {
@@ -35,7 +34,6 @@ class TicketController extends Controller
                 'department' => $ticket->department()->first()->name];
         }
         return view('tickets.show_tickets')->with([
-            'user' => Auth::user(),
             'tickets' => $tickets_info
         ]);
     }
@@ -75,58 +73,9 @@ class TicketController extends Controller
 
         return view('tickets.create_ticket')
             ->with([
-                'user' => Auth::user(),
                 'services' => $services,
                 'departments' => $departments
             ]);
-    }
-
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param Ticket $ticket
-     * @return Application|Factory|View
-     */
-    public function manage(Ticket $ticket)
-    {
-        $tickets_info = ['id' => $ticket->id,
-            'subject' => $ticket->subject,
-            'content' => $ticket->content,
-            'priority' => $ticket->priority,
-            'active' => $ticket->active ? "فعال" : "غیرفعال",
-            'customer' => $ticket->customer()->first()->name,
-            'service' => $ticket->service()->first()->title,
-            'department' => $ticket->department()->first()->name];
-        $departments = Department::all();
-        return view('tickets.manage_ticket')->with([
-            'user' => Auth::user(),
-            'ticket' => $tickets_info,
-            'departments' => $departments
-        ]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param ChangeDepartemntTicketRequest $request
-     * @return RedirectResponse
-     */
-    public function change_department(ChangeDepartemntTicketRequest $request)
-    {
-        $data = $request->validated();
-        $ticket = Ticket::find($data['ticket']);
-        $ticket->department_id = $data['department'];
-        $ticket->save();
-        return redirect()->back();
-    }
-
-    public function deactivate(Request $request)
-    {
-        $ticket = Ticket::find($request->input('ticket'));
-        $ticket->active = false;
-        $ticket->save();
-        return redirect()->back();
     }
 
     /**
