31cd7240632e62b7341b030a04767b8c77481eae
Ali Mashyekhi <alim201381@gmail.com>
2022-09-10 00:18:44 -0700

    Fix columns and show 2 input in a row

--- resources/views/tickets/replies/show_ticket_replies.blade.php
+++ resources/views/tickets/replies/show_ticket_replies.blade.php
@@ -39,138 +39,170 @@
                         </div>
 
                         <div class="card-body">
-                            <div class="row">
-                                <div class="col-lg-7">
-                                    <form>
+                            <form class="col-lg-12">
+                                <div class="row">
+                                    <div class="col-lg-6">
                                         <div class="form-group">
                                             <label for="service">{{ __('فرستنده') }}</label>
                                             <input id="service" class="form-control" type="text" readonly
                                                    value="{{ __($ticket['customer']) }}">
                                         </div>
+                                    </div>
+                                    <div class="col-lg-6">
                                         <div class="form-group">
                                             <label for="service">{{ __('سرویس') }}</label>
                                             <input id="service" class="form-control" type="text" readonly
                                                    value="{{ __($ticket['service']) }}">
                                         </div>
+                                    </div>
+                                    <div class="col-lg-6">
                                         <div class="form-group">
                                             <label for="department" class="form-label">{{ __('دپارتمان') }}</label>
                                             <input id="department" class="form-control"
                                                    value="{{ __($ticket['department']) }}" type="text"
                                                    readonly>
                                         </div>
+                                    </div>
+                                    <div class="col-lg-6">
                                         <div class="form-group">
                                             <label for="subject" class="form-label">{{ __('موضوع') }}</label>
                                             <input id="subject" class="form-control" type="text" readonly
                                                    value="{{ __($ticket['subject']) }}">
                                         </div>
-                                        <div class="form-group">
-                                            <label for="content" class="form-label">{{ __('متن') }}</label>
-                                            <textarea id="content" name="content" class="form-control" rows="3"
-                                                      readonly>
-                                                {{ __($ticket['content']) }}
-                                            </textarea>
-                                        </div>
-                                    </form>
-                                </div>
-                                <div class="card card-secondary card-replies">
-                                    <div class="card-header">
-                                        <h3 class="card-title">
-                                            پاسخ ها
-                                        </h3>
                                     </div>
-                                    <div class="card-body">
-                                        @foreach($replies as $reply)
-                                            <div class="row {{ __($reply['justify']) }}">
-                                                <div class="col-lg-6">
-                                                    <div class="reply" id="{{ __($reply['id']) }}">
-                                                        <div class="reply-header">
-                                                            <h5 class="reply-title">
-                                                                {{ __($reply['sender']->name) }}
-                                                            </h5>
-                                                            <div class="dropdown">
-                                                                <button class="reply-more-info dropdown-toggle"
-                                                                        type="button" id="dropdownMenuButton"
-                                                                        data-toggle="dropdown"
-                                                                        aria-haspopup="true"
-                                                                        aria-expanded="false">
-                                                                    ...
-                                                                </button>
-                                                                <div class="dropdown-menu"
-                                                                     aria-labelledby="dropdownMenuButton">
-                                                                    <a class="dropdown-item" href="#">یچیزی</a>
-                                                                    <a class="dropdown-item" href="#">کپی</a>
-                                                                    <a class="dropdown-item report-reply" href="#"
-                                                                       data-toggle="modal"
-                                                                       data-target="#reportReplyModal">گزارش</a>
-                                                                </div>
-                                                            </div>
-
-
-                                                        </div>
-                                                        <div class="reply-body">
-                                                            {{ __($reply['content']) }}
-                                                        </div>
+                            </form>
+                        </div>
+                        <div class="card card-secondary card-replies">
+                            <div class="card-header">
+                                <h3 class="card-title">
+                                    پاسخ ها
+                                </h3>
+                            </div>
+                            <div class="card-body">
+                                <div class="row justify-content-start">
+                                    <div class="col-lg-12">
+                                        <div class="reply">
+                                            <div class="reply-header">
+                                                <h5 class="reply-title">
+                                                    {{ __('متن تیکت') }}
+                                                </h5>
+                                                <div class="dropdown">
+                                                    <button class="reply-more-info dropdown-toggle"
+                                                            type="button" id="dropdownMenuButton"
+                                                            data-toggle="dropdown"
+                                                            aria-haspopup="true"
+                                                            aria-expanded="false">
+                                                        ...
+                                                    </button>
+                                                    <div class="dropdown-menu"
+                                                         aria-labelledby="dropdownMenuButton">
+                                                        <a class="dropdown-item" href="#">یچیزی</a>
+                                                        <a class="dropdown-item" href="#">کپی</a>
+                                                        <a class="dropdown-item report-reply" href="#"
+                                                           data-toggle="modal"
+                                                           data-target="#reportReplyModal">گزارش</a>
                                                     </div>
                                                 </div>
+
+
                                             </div>
-
-                                        @endforeach
-                                    </div>
-                                    <div class="add-reply">
-                                        <form class="add-reply" method="post" action="{{ route('replies.create') }}">
-                                            @csrf
-                                            <input type="hidden" name="ticket" value="{{ __($ticket['id']) }}">
-                                            <div id="editor">
-                                                <p>متن پاسخ خود را بنویسید ...</p>
+                                            <div class="reply-body">
+                                                {{ __($ticket['content']) }}
                                             </div>
-
-
-                                            <button class="btn btn-primary btn-reply-send">
-                                                ارسال
-                                            </button>
-                                        </form>
+                                        </div>
                                     </div>
                                 </div>
+                                @foreach($replies as $reply)
+                                    <div class="row {{ __($reply['justify']) }}">
+                                        <div class="col-lg-6">
+                                            <div class="reply" id="{{ __($reply['id']) }}">
+                                                <div class="reply-header">
+                                                    <h5 class="reply-title">
+                                                        {{ __($reply['sender']->name) }}
+                                                    </h5>
+                                                    <div class="dropdown">
+                                                        <button class="reply-more-info dropdown-toggle"
+                                                                type="button" id="dropdownMenuButton"
+                                                                data-toggle="dropdown"
+                                                                aria-haspopup="true"
+                                                                aria-expanded="false">
+                                                            ...
+                                                        </button>
+                                                        <div class="dropdown-menu"
+                                                             aria-labelledby="dropdownMenuButton">
+                                                            <a class="dropdown-item" href="#">یچیزی</a>
+                                                            <a class="dropdown-item" href="#">کپی</a>
+                                                            <a class="dropdown-item report-reply" href="#"
+                                                               data-toggle="modal"
+                                                               data-target="#reportReplyModal">گزارش</a>
+                                                        </div>
+                                                    </div>
+
+
+                                                </div>
+                                                <div class="reply-body">
+                                                    {{ __($reply['content']) }}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                @endforeach
+                            </div>
+                            <div class="add-reply">
+                                <form class="add-reply" method="post" action="{{ route('replies.create') }}">
+                                    @csrf
+                                    <input type="hidden" name="ticket" value="{{ __($ticket['id']) }}">
+                                    <div id="editor">
+                                        <p>متن پاسخ خود را بنویسید ...</p>
+                                    </div>
+
+
+                                    <button class="btn btn-primary btn-reply-send">
+                                        ارسال
+                                    </button>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Modal -->
-        <div class="modal fade" id="reportReplyModal" tabindex="-1" aria-labelledby="reportReplyModal"
-             aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="reportReplyModal">گزارش</h5>
-                        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p/>در صورت مشاهده محتوای خلاف قوانین یا مواردی که نیاز به اطلاع رسانی به مدیر دارد میتوانید از
-                        این قسمت گزارش دهید.
-                        <form action="{{ route('replies.reports.create')  }}" method="POST"
-                              id="modal-form">
-                            @csrf
+    </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="reportReplyModal" tabindex="-1" aria-labelledby="reportReplyModal"
+         aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="reportReplyModal">گزارش</h5>
+                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p/>در صورت مشاهده محتوای خلاف قوانین یا مواردی که نیاز به اطلاع رسانی به مدیر دارد میتوانید از
+                    این قسمت گزارش دهید.
+                    <form action="{{ route('replies.reports.create')  }}" method="POST"
+                          id="modal-form">
+                        @csrf
 
-                            <input type="hidden" id="reply-id" name="reply">
-                            <input type="hidden" id="ticket" name="ticket" value="{{ __($ticket['id']) }}">
-                            <div class="form-group">
-                                <label for="content">
-                                    متن گزارش
-                                </label>
-                                <textarea class="form-control" name="content" id="content" rows="5"></textarea>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">بستن</button>
+                        <input type="hidden" id="reply-id" name="reply">
+                        <input type="hidden" id="ticket" name="ticket" value="{{ __($ticket['id']) }}">
+                        <div class="form-group">
+                            <label for="content">
+                                متن گزارش
+                            </label>
+                            <textarea class="form-control" name="content" id="content" rows="5"></textarea>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">بستن</button>
 
-                        <button type="submit" class="btn btn-primary" id="modal-form-submit">
-                            ارسال
-                        </button>
+                    <button type="submit" class="btn btn-primary" id="modal-form-submit">
+                        ارسال
+                    </button>
 
-                    </div>
                 </div>
             </div>
         </div>
@@ -180,7 +212,13 @@
 
         ClassicEditor
             .create(document.querySelector('#editor'), {
-                toolbar: ['bold', 'italic', 'alignment', 'bulletedList', 'numberedList'],
+                language: {
+                    // The UI will be English.
+                    ui: 'en',
+                    // But the content will be edited in Arabic.
+                    content: 'ar'
+                },
+                toolbar: ['bold', 'italic', 'bulletedList', 'numberedList'],
                 list: {
                     properties: {
                         styles: true,
@@ -188,6 +226,9 @@
                         reversed: true
                     }
                 },
+            })
+            .then(editor => {
+                window.editor = editor;
             })
             .catch(error => {
                 console.error(error);
