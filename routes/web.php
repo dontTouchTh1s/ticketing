021fc9149d11aee9ae3401cd44d8a4713d8eda37
Ali Mashyekhi <alim201381@gmail.com>
2022-09-12 05:31:18 -0700

    Add notifications routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Ticket\CreateTicketController;
-use App\Http\Controllers\Ticket\ManageTicketController;
-use App\Http\Controllers\Ticket\Reply\ReplyController;
-use App\Http\Controllers\Ticket\Report\ReportController;
-use App\Http\Controllers\Ticket\TicketController;
+use App\Http\Controllers\Notifications\NotificationController;
+use App\Http\Controllers\Tickets\CreateTicketController;
+use App\Http\Controllers\Tickets\ManageTicketController;
+use App\Http\Controllers\Tickets\Reply\ReplyController;
+use App\Http\Controllers\Tickets\Report\ReportController;
+use App\Http\Controllers\Tickets\TicketController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -34,19 +35,25 @@ Route::prefix('admin')->middleware('auth')->group(function () {
         Route::get('manage{ticket}', [ManageTicketController::class, 'index'])->name('tickets.manage');
         Route::patch('change-department', [ManageTicketController::class, 'change_department'])->name('tickets.change_department');
         Route::patch('deactivate', [ManageTicketController::class, 'deactivate'])->name('tickets.deactivate');
-        // Ticket replies routes
+        // Tickets replies routes
         Route::prefix('replies')->group(function () {
             Route::get('{ticket}', [ReplyController::class, 'index'])->name('replies');
             Route::post('create', [ReplyController::class, 'store'])->name('replies.create');
         });
-        // Ticket reports routes
+        // Tickets reports routes
         Route::prefix('reports')->group(function () {
             Route::get('/', [ReportController::class, 'index'])->name('reports');
             Route::post('create', [ReportController::class, 'store'])->name('reports.create');
         });
-
-
     });
+    // Notifications routes
+    Route::prefix('notifications')->group(function () {
+        Route::get('', [NotificationController::class, 'index'])->name('notification');
+        Route::get('', [CreateNotificationController::class, 'create'])->name('notification.create');
+        Route::post('', [CreateNotificationController::class, 'store'])->name('notification.store');
+        Route::delete('', [DeleteNotificationController::class])->name('notification.store');
+    });
+    // Services Routes
 });
 
 Auth::routes();
