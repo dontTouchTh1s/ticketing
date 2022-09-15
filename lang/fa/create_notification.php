d358a50f65188ddeac6697c8a6e6274bcdfe1c2c
dontTouchThis <alim201381@gmail.com>
2022-09-16 01:17:29 +0430

    Add notification lang files

---
+++ lang/fa/create_notification.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+
+    'page-title' => 'ایجاد اعلان',
+    'main-card-title' => 'ایجاد اعلان جدید',
+    'description' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
---
+++ lang/fa/show_notification.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+
+    'page-title' => 'اعلان ها',
+    'main-card-title' => 'اعلان های ایجاد شده',
+
+];
