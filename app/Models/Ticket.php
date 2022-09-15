9daa80ddcf81f3c87d09d7d3e9b494d67c4f6f3c
dontTouchThis <alim201381@gmail.com>
2022-09-16 01:20:48 +0430

    Remove @method create

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -5,13 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * @method static create(array $ticket)
- */
+
 class Ticket extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'customer_id',
         'service_id',
@@ -22,6 +25,7 @@ class Ticket extends Model
         'subject',
         'content'
     ];
+
 
     public function service()
     {
