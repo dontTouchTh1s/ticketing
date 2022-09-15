bdcdc13049a5cc2d3e0c8ab9ef0ec5f0c00c4d72
dontTouchThis <alim201381@gmail.com>
2022-09-16 01:17:00 +0430

    Add fa lang to application, change application default lang

--- app/Http/Requests/Notifications/UpdateNotificationRequest.php
+++ app/Http/Requests/Notifications/UpdateNotificationRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Notifications;
 
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -24,7 +24,9 @@ class UpdateNotificationRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required',
+            'body' => 'required',
+            'type' => 'required|integer'
         ];
     }
 }
---
+++ lang/fa/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'این مشخصات با اطلاعات ما همخوانی ندارد.',
+    'password' => 'رمز وارد شده صحیح نمی باشد.',
+    'throttle' => 'تعداد دفعات زیادی سعی شده، لطفا در :second تلاش کنید.',
+
+];
